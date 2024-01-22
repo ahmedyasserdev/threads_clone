6678cfe140f1f496ab1c7a534de7b7eb05752610
Ahmed Yasser <ahmedyasser3033@gmail.com>
2024-01-22 19:54:23 +0200

    fetched threads in the home page and it's single page\

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,4 @@
-'use server'
+"use server";
 
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
@@ -6,28 +6,100 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
 interface Params {
-    text : string ;
-    author : string ;
-    communityId : string | null ;
-    path : string ;
-
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
 }
 
-export async function createThread ({text , author  , communityId , path} : Params) {
-   try {
-    connectToDB()
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  try {
+    connectToDB();
     const createdThread = await Thread.create({
-        text ,
-        author ,
-        community :  null ,
-
-    })
+      text,
+      author,
+      community: null,
+    });
 
     //update user model
-        await User.findByIdAndUpdate(author , {$push : { threads : createdThread._id }})
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
 
-        revalidatePath(path)
-   } catch (error : any) {
-        throw new  Error(`failed to create new  thread ${error.message}`)
-   }
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`failed to create new  thread ${error.message}`);
+  }
+}
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+  const skipedAmount = (pageNumber - 1) * pageSize;
+  const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipedAmount)
+    .limit(pageSize)
+    .populate({
+      path: "author",
+      model: User,
+    })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalThreadsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const threads = await threadsQuery.exec();
+
+  const isNext = totalThreadsCount > skipedAmount + threads.length;
+
+  return { threads, isNext };
+}
+
+export async function fetchThreadById(id : string) {
+  connectToDB();
+  try {
+    const thread = await Thread.findById(id).populate({
+      path : "author",
+      model : User ,
+      select : "_id name image id"
+    })
+    .populate({
+      path : 'children',
+      populate : [{
+        path : "author",
+        model : User,
+      select : "_id name image id parentId "
+
+      },
+
+      {
+        path : "children" ,
+        model : Thread ,
+        populate : {
+          path : "author",
+        model : User,
+      select : "_id name image id parentId "
+        }
+      }
+    
+    ]
+    }).exec()
+
+    return thread
+  } catch (error: any) {
+    throw new Error(`failed to create fetch  thread by id ${error.message}`);
+  }
 }
