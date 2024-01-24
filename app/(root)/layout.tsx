4301b68f6a2b18ef8c31fc8416cae9434dff7c22
Ahmed Yasser <ahmedyasser3033@gmail.com>
2024-01-24 22:38:45 +0200

    updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
               <div className="w-full max-w-4xl">{children}</div>
             </section>
 
-            <RightSidebar />
           </main>
           <Bottombar />
         </body>
